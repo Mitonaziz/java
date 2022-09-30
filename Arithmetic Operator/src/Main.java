b413cf7268b8f4feccdc879eccf943ade328d3d0
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2022-09-30 22:58:04 +0600

    2num sum

---
+++ Arithmetic Operator/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       //there are 5 number of Arithmetic operator ( + - * / % )
+        int num1,num2,result;
+        num2=20;
+        num1=30;
+        result =num1+num2;
+        System.out.println("sum valu is "+result);
+
+
+    }
+}

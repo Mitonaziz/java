c0ce88b935f71cf13625005d75a34fce948d60e1
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2022-09-30 21:46:01 +0600

    Format Specifier

---
+++ Format Specifier 02/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+    boolean b=true;
+    int i=123;
+    short s=3255;
+    float f=3.1f;
+    double d=122.151;
+
+
+        System.out.printf("integer i= %d\n",i);
+        System.out.printf("flot f= %.2f\n",f);
+
+    }
+}

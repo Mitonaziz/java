41bae63665c1a0adc473398a9ae7f2aa3c7f689e
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2022-09-30 23:16:54 +0600

    input

--- Arithmetic Operator/src/Main.java
+++ Arithmetic Operator/src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
        //there are 5 number of Arithmetic operator ( + - * / % )
         int num1,num2,result;
-        num2=20;
-        num1=30;
+       Scanner scan= new Scanner(System.in);
+       System.out.print("Enter two numbers = ");
+       num1=scan.nextInt();
+        num2=scan.nextInt();
         result =num1+num2;
         System.out.println("sum valu is "+result);
         result =num1-num2;

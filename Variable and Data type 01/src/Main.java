8107cfb9e29527589d5d25cfe58ec2c25af692da
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2022-09-30 21:33:32 +0600

    /dinamic initialization

---
+++ Variable and Data type 01/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean b;
+        char c;
+        short s =3224; //dinamic initialization
+        int i=12345;
+        float f;
+        double d=10.2; //valu decler
+        b=true;
+        c='a';//valu decler
+        System.out.println("b="+b);
+        System.out.println("c="+ c); //valu initialage
+        System.out.println("s="+ s);
+        System.out.println("d"+d);
+    }
+}

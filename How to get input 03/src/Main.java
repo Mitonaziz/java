0c8b0a1ed9ebf66d4c0b8d7a4205f68f610aea71
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2022-09-30 21:54:46 +0600

    now i input a number

---
+++ How to get input 03/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        int num ;
+        System.out.print("Enter you Interger number=");
+        num=input.nextInt();
+       System.out.println("Number ="+num);
+    }
+}

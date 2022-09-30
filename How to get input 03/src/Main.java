eb5e20b9450c42762e443bf064e60b5b6d8e35b9
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2022-09-30 22:21:49 +0600

    String valu input

--- How to get input 03/src/Main.java
+++ How to get input 03/src/Main.java
@@ -4,8 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
         int num ;
+        String Name;
+        Name="Md Azizur Rahman";
         System.out.print("Enter you Interger number=");
         num=input.nextInt();
+        Name=input.nextLine();
        System.out.println("Number ="+num);
+       System.out.print("Enter your name =");
+       Name=input.nextLine();
+
+       System.out.println("Name is ="+Name);
     }
 }

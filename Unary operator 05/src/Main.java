636d0e403bb95d93224620eca1bbcc96b6564fe0
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2022-10-04 23:58:41 +0600

    //Unary +  and Unary -

--- Unary operator 05/src/Main.java
+++ Unary operator 05/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Unary +  and Unary -
+        int x =-10;
+        int Result;
+        Result= +x;//unary +
+        System.out.println("Result = "+Result);
+        Result= -x;//Unary -
+        System.out.println("Result="+Result);
     }
 }
